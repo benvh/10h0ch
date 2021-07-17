7e8a62989832412acc9187c4ca4e833412ae0da6
benvh <benvh@coder.town>
2021-07-17 23:12:53 +0200

    make normal mode / "none" tool display image dimensions

--- src/tools/none.c
+++ src/tools/none.c
@@ -1,9 +1,12 @@
 #include "tool.h"
+#include "image.h"
 
-static char* status_bar_text = "[normal]";
+//static char* status_bar_text = "[normal]";
+static char status_bar_buff[64];
 
 char* tools_mode_none_provide_status_bar_text() {
-    return status_bar_text;
+    sprintf(status_bar_buff, "[normal] | (%d x %d)", img_orig_surface->w, img_orig_surface->h);
+    return status_bar_buff;
 }
 
 tool_t tools_tool_none = {
